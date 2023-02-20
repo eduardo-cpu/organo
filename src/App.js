96acfc79f6a3a20428e5db83199c7fe014856070
Eduardo <eduardosantossm@hotmail.com>
2023-02-20 11:02:03 -0300

    organo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Banner from './componentes/Banner/Banner';
+import Formulario from './componentes/Formulario';
+import Rodape from './componentes/Rodape';
+import Time from './componentes/Time';
 
 function App() {
+  
+  const times = [
+    {
+      nome:'Alunos UFSM',
+      corPrimaria:'#57C278',
+      corSecundaria:'#D9F7E9'
+    },
+    {
+      nome:'Alunos UFN',
+      corPrimaria:'#82CFFA',
+      corSecundaria:'#E8F8FF'
+    },
+    {
+      nome:'Maiores horas no lol',
+      corPrimaria:'#A6D157',
+      corSecundaria:'#F0F8E2'
+    },
+    {
+      nome:'Maiores horas no CS ',
+      corPrimaria:'#E06B69',
+      corSecundaria:'#FDE7E8'
+    }
+
+  ]
+
+  const [colaboradores, setColaboradores] = useState([])
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    setColaboradores([...colaboradores, colaborador])
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner/>
+     <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+    {times.map(time =><Time 
+      key={time.nome} 
+      nome={time.nome} 
+      corPrimaria={time.corPrimaria} 
+      corSecundaria={time.corSecundaria}
+      colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome )}
+      />)}
+     <Rodape/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react'
+import './Banner.css'
+function Banner(){
+    return (
+        <Fragment>
+            <header className="banner">
+            <img src="/imagens/banner.png" alt= "Banner principal"/>
+        </header>
+        </Fragment>
+    )
+
+}
+
+export default Banner
